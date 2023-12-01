b727d22ceec7ef43c131c6686b3e7934357d61b6
ronald2117 <abel.ronald2117@gmail.com>
2023-12-01 10:20:24 +0800

    Add initial java filed

---
+++ AboutPage.java
@@ -0,0 +1,10 @@
+public class AboutPage {
+    int myNum;
+
+    public AboutPage {
+        myNum = num;
+    }
+    public static void printAboutP() {
+        System.out.println("This is the about page");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        AboutPage aboutPage = new AboutPage();
+
+        aboutPage.printAboutP();
+    }
+}
